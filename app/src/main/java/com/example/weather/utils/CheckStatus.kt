d0fb74eab7e951ab6def4b91912825ca39ca39b4
merovenkov <matvei.erovenkov@gmail.com>
2020-10-06 11:46:54 +0300

    changed function "is Network Available". added check for android 23 and earlier.

--- app/src/main/java/com/example/weather/utils/CheckStatus.kt
+++ app/src/main/java/com/example/weather/utils/CheckStatus.kt
@@ -1,14 +1,31 @@
 package com.example.weather.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
+import android.os.Build
+import androidx.core.net.ConnectivityManagerCompat
+import com.example.weather.R
+import com.example.weather.view.recycler.toast.ShowToast
 
 object CheckStatus {
+    @SuppressLint("ServiceCast")
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetwork
+        val isActive: Boolean
 
-        return activeNetworkInfo != null
+        isActive = if (Build.VERSION.SDK_INT >= 23) {
+            connectivityManager.activeNetwork != null
+        } else {
+            ConnectivityManagerCompat.isActiveNetworkMetered(connectivityManager)
+        }
+
+        if (!isActive) {
+            ShowToast.getToast(
+                context,
+                context.resources.getString(R.string.no_internet_access))
+        }
+        return isActive
     }
 }
