723731c95d889e69d05715447f59589bab1d64c5
merovenkov <matvei.erovenkov@gmail.com>
2020-10-19 18:54:44 +0300

    changing NetworkService

--- app/src/main/java/com/example/weather/utils/api/NetworkService.kt
+++ app/src/main/java/com/example/weather/utils/api/NetworkService.kt
@@ -3,12 +3,13 @@ package com.example.weather.utils.api
 import com.example.weather.utils.api.interfaces.IWeatherApi
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.InputStream
 
-class NetworkService() {
+class NetworkService(certificate: InputStream) {
     private val BASE_URL = "https://api.openweathermap.org"
-    private val hostName = "api.openweathermap.org"
     private val protocol = "SSL"
     private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     private var retrofit: Retrofit
@@ -16,7 +17,7 @@ class NetworkService() {
     init {
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(OkHttpFactory.getCustomOkHttpClient(protocol, hostName))
+            .client(OkHttpFactory.getCustomOkHttpClient(protocol, certificate))
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
