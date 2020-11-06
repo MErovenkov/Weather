29c8f60b676a844e60b5d4469896f71ead95239c
merovenkov <matvei.erovenkov@gmail.com>
2020-11-06 06:10:39 +0300

    added abstract EventStatusViewModel.kt

---
+++ app/src/main/java/com/example/weather/viewmodel/EventStatusViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.weather.viewmodel
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import com.example.weather.repository.Repository
+
+abstract class EventStatusViewModel(repository: Repository): ViewModel() {
+
+    private var eventStatus = MediatorLiveData<Int>()
+
+    init {
+        eventStatus.addSource(repository.getEventStatus()) {
+            eventStatus.postValue(it.getContentIfNotHandled())
+        }
+    }
+
+    fun getEvent() = eventStatus
+}
