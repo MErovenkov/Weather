fab64c6563b9d6a3ecea0337acece5dc727cf9dc
merovenkov <matvei.erovenkov@gmail.com>
2020-11-06 06:16:19 +0300

    changing in module

--- app/src/main/java/com/example/weather/di/module/ViewModelModule.kt
+++ app/src/main/java/com/example/weather/di/module/ViewModelModule.kt
@@ -1,13 +1,10 @@
 package com.example.weather.di.module
 
-import android.app.Application
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
-import com.example.weather.dao.OrmLiteHelper
-import com.example.weather.api.WeatherData
+import com.example.weather.repository.Repository
 import com.example.weather.di.qualifier.ActivityContext
-import com.example.weather.di.qualifier.ApplicationContext
 import com.example.weather.di.scope.ActivityScope
 import com.example.weather.viewmodel.DetailedWeatherViewModel
 import com.example.weather.viewmodel.ViewModelFactory
@@ -20,10 +17,8 @@ class ViewModelModule {
 
     @Provides
     @ActivityScope
-    fun viewModelFactory(@ApplicationContext applicationContext: Context,
-                         dataBaseHelper: OrmLiteHelper,
-                         weatherData: WeatherData): ViewModelFactory {
-        return ViewModelFactory(applicationContext as Application, dataBaseHelper, weatherData)
+    fun viewModelFactory(repository: Repository): ViewModelFactory {
+        return ViewModelFactory(repository)
     }
 
     @Provides
