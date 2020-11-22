24aa26b761e05a3331d32f31015f04ab4575cd01
merovenkov <matvei.erovenkov@gmail.com>
2020-11-23 02:55:56 +0300

    added functions to display the current location and uses extension functions to exceptions in Repository.kt

--- app/src/main/java/com/example/weather/repository/Repository.kt
+++ app/src/main/java/com/example/weather/repository/Repository.kt
@@ -1,19 +1,13 @@
 package com.example.weather.repository
 
-import android.util.Log
 import com.example.weather.repository.api.WeatherData
 import com.example.weather.repository.dao.OrmLiteHelper
 import com.example.weather.model.WeatherCity
-import com.example.weather.utils.EventStatus
-import com.example.weather.utils.Resource
-import com.example.weather.utils.getWeatherCities
-import com.example.weather.utils.getWeatherCityByName
+import com.example.weather.utils.*
+import com.example.weather.utils.extension.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
-import java.net.ConnectException
-import java.sql.SQLException
-import javax.net.ssl.SSLException
 
 class Repository(private val dataBaseHelper: OrmLiteHelper,
                  private val weatherData: WeatherData) {
@@ -26,73 +20,80 @@ class Repository(private val dataBaseHelper: OrmLiteHelper,
 
     fun createWeatherCity(nameCity: String) = flow {
             val newWeatherCity = withContext(Dispatchers.IO) {
-                    weatherData.getWeatherCity(nameCity)
+                weatherData.getWeatherCity(nameCity)
             }
+
             dataBaseHelper.createWeatherCity(newWeatherCity)
             emit(Resource(EventStatus.CITY_ADDED, dataBaseHelper.getWeatherCities()))
-        }.catch { e ->
-            when(e) {
-                is NullPointerException -> {
-                    Log.w("$e nameCity: $nameCity", e.stackTraceToString())
-                    emit(Resource(EventStatus.CITY_NOT_FOUND, null))
-                }
-
-                is SQLException -> {
-                    Log.w("$e nameCity: $nameCity", e.stackTraceToString())
-                    emit(Resource(EventStatus.CITY_EXIST))
-                }
-            }
-    }
+    }.createWeatherException(nameCity)
 
     fun updateWeatherCity(weatherCity: WeatherCity) = flow {
-            val newWeatherCity = withContext(Dispatchers.IO) {
-                weatherData.getUpdateWeatherCity(weatherCity)
-            }
-            dataBaseHelper.updateWeatherCity(newWeatherCity)
-            emit(Resource(EventStatus.CITY_WEATHER_DATA_UPDATED,
-                dataBaseHelper.getWeatherCityByName(weatherCity.nameCity)))
-        }.catch { e ->
-            when(e) {
-                is ConnectException -> {
-                    Log.e(e.toString(), e.stackTraceToString())
-                    emit(Resource(EventStatus.LOST_INTERNET_ACCESS))
-                }
+        val newWeatherCity = withContext(Dispatchers.IO) {
+            weatherData.getUpdateWeatherCity(weatherCity)
+        }
 
-                is SSLException -> {
-                    Log.w(e.toString(), e.stackTraceToString())
-                    emit(Resource(EventStatus.CITY_WEATHER_UPDATE_FAILED))
-                }
-            }
-    }
+        dataBaseHelper.updateWeatherCity(newWeatherCity)
+        emit(Resource(EventStatus.CITY_WEATHER_DATA_UPDATED,
+            dataBaseHelper.getWeatherCityByName(weatherCity.nameCity)))
+    }.connectException()
+     .sslUpdateException()
 
     fun updateAllCitiesWeather() = flow {
             val weatherCityList = withContext(Dispatchers.IO) {
                 weatherData.getUpdatedWeatherCityList(dataBaseHelper.getWeatherCities())
             }
+
             if (weatherCityList.isNotEmpty()) {
                 dataBaseHelper.updateAllCitiesWeather(weatherCityList)
                 emit(Resource(EventStatus.CITY_WEATHER_DATA_UPDATED, dataBaseHelper.getWeatherCities()))
             }
-        }.catch { e ->
-            when (e) {
-                is ConcurrentModificationException -> {
-                    Log.e(e.toString(), e.stackTraceToString())
-                    emit(Resource(EventStatus.CITY_WEATHER_UPDATE_FAILED))
-                }
-
-                is ConnectException -> {
-                    Log.w(e.toString(), e.stackTraceToString())
-                    emit(Resource(EventStatus.LOST_INTERNET_ACCESS))
-                }
-
-                is SSLException -> {
-                    Log.w(e.toString(), e.stackTraceToString())
-                    emit(Resource(EventStatus.CITY_WEATHER_UPDATE_FAILED))
-                }
-            }
-    }
+    }.connectException()
+     .sslUpdateException()
+     .concurrentModificationException()
 
     fun deletedWeatherCity(weatherCity: WeatherCity) {
         dataBaseHelper.deletedWeatherCity(weatherCity)
     }
+
+    /**
+     * Current Location
+     * */
+    fun getCurrentLocationWeather(): WeatherCity =
+        dataBaseHelper.getCurrentLocationWeather()
+
+    fun createWeatherCurrentLocation(nameCity: String) = flow {
+            val newWeatherCity = withContext(Dispatchers.IO) {
+                weatherData.getWeatherCity(nameCity)
+            }
+
+            newWeatherCity.isCurrentLocation = true
+            dataBaseHelper.createWeatherCity(newWeatherCity)
+
+            emit(Resource(dataBaseHelper.getWeatherCities()))
+    }.connectException()
+
+    fun updateWeatherCurrentLocation(nameCity: String) = flow {
+            val weatherCurrentLocation = getCurrentLocationWeather()
+
+            if (weatherCurrentLocation.nameCity == nameCity) {
+                val newWeatherCity = withContext(Dispatchers.IO) {
+                    weatherData.getUpdateWeatherCity(weatherCurrentLocation)
+                }
+
+                dataBaseHelper.updateWeatherCity(newWeatherCity)
+            } else {
+                val newWeatherCity = withContext(Dispatchers.IO) {
+                    weatherData.getWeatherCity(nameCity)
+                }
+
+                newWeatherCity.isCurrentLocation = true
+
+                dataBaseHelper.deletedWeatherCity(weatherCurrentLocation)
+                dataBaseHelper.createWeatherCity(newWeatherCity)
+            }
+
+            emit(Resource(dataBaseHelper.getWeatherCities()))
+    }.connectException()
+     .sslUpdateException()
+     .concurrentModificationException()
 }
