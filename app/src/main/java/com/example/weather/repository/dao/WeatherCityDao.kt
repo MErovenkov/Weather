f0e3b136cfc90d0b2e2b2c0c4503cd28850815da
merovenkov <matvei.erovenkov@gmail.com>
2020-11-23 02:40:49 +0300

    added request for take weatherCurrentLocation in WeatherCityDao.kt

--- app/src/main/java/com/example/weather/repository/dao/WeatherCityDao.kt
+++ app/src/main/java/com/example/weather/repository/dao/WeatherCityDao.kt
@@ -1,6 +1,7 @@
 package com.example.weather.repository.dao
 
 import com.example.weather.model.WeatherCity
+import com.example.weather.utils.DBNaming
 import com.j256.ormlite.dao.BaseDaoImpl
 import com.j256.ormlite.support.ConnectionSource
 import java.sql.SQLException
@@ -15,6 +16,13 @@ class WeatherCityDao @Throws(SQLException::class) constructor(connectionSource: 
     fun getWeatherCityByName(nameCity: String): WeatherCity {
         return this.queryForFirst(
             this.queryBuilder()
-                .where().eq("name_city", nameCity).prepare())
+                .where().eq(DBNaming.WeatherCityEntry.COLUMN_NAME_CITY, nameCity)
+                .and().eq(DBNaming.WeatherCityEntry.COLUMN_IS_CURRENT_LOCATION, false).prepare())
+    }
+
+    fun getWeatherCurrentLocation(): WeatherCity {
+        return this.queryForFirst(
+            this.queryBuilder()
+                .where().eq(DBNaming.WeatherCityEntry.COLUMN_IS_CURRENT_LOCATION, true).prepare())
     }
 }
